0ca4beb5e7a567528bb1b6d076851ac12f0b4238
Fedor Dikarev <fedor.dikarev@gmail.com>
2020-03-09 11:01:30 +0300

    Extra stat

--- ya_forms_extra_stat.py
+++ ya_forms_extra_stat.py
@@ -18,19 +18,28 @@ business_talks = [
     'Кто должен общаться с бизнесом насчет стоимости эксплуатации продукта?',
 ]
 
+cto_name = "CTO/CIO/IT Менеджер"
+sys_name = "Системный администратор"
+dev_name = "Разработчик"
 arch_talks = [
-    'Кто выбирает стэк технологий продукта? (язык разработки, архитектура, монолит/микросервисы, etc)',
-    'Кто выбирает DB-стэк продукта? (всякие уровни кэширования и хранения пусть будут здесь же)',
-    'Кто выбирает среду исполнения продукта? (железо: свое/арендованное, виртуалки, контейнеры; облака: одно или'\
+    {"q":'Кто выбирает стэк технологий продукта? (язык разработки, архитектура, монолит/микросервисы, etc)',
+     "extra": [cto_name, dev_name]},
+    {"q":'Кто выбирает DB-стэк продукта? (всякие уровни кэширования и хранения пусть будут здесь же)',
+     "extra": [cto_name, "DBA"]},
+    {"q":'Кто выбирает среду исполнения продукта? (железо: свое/арендованное, виртуалки, контейнеры; облака: одно или '\
         'несколько разных)',
-    'Кто выбирает системы и источники мониторинга и логирования?',
-    'А за безопасность кто отвечает?',
-    'Делать новое все любят, а кто за gargabe collect отвечает? (удалить ненужные ветки в репо, грохнуть тестовые'\
+     "extra": [cto_name, sys_name]},
+    {"q":'Кто выбирает системы и источники мониторинга и логирования?',
+     "extra": [sys_name]},
+    {"q":'А за безопасность кто отвечает?',
+     "extra": [sys_name]},
+    {"q":'Делать новое все любят, а кто за gargabe collect отвечает? (удалить ненужные ветки в репо, грохнуть тестовые '\
         'базы)',
-    'Garbage collecting провели, все старое удалили, а вот за backup и восстановление из бэкапа кто ответит?',
+     "extra": [dev_name, "DevOps"]},
+    {"q":'Garbage collecting провели, все старое удалили, а вот за backup и восстановление из бэкапа кто ответит?',
+     "extra": [sys_name]},
 ]
 
-cto_name = "CTO/CIO/IT Менеджер"
 
 def extra_stats(df):
     def count_me_in(column_name, extra_persons=[]):
@@ -56,12 +65,21 @@ def extra_stats(df):
     print("Bad is me")
     print(bad_is_me)
 
-    print("=== Разговры с бизнесом ===")
+    print("=== Разговоры с бизнесом ===")
     for talk in business_talks:
         result_me, result_me_with_cto = count_me_in(talk, [cto_name])
         print(talk)
         print("Me ", result_me)
         print("Me and {} ".format(cto_name), result_me_with_cto)
+        print()
+    print("=== Разговоры за Архитектуру ===")
+    for talk in arch_talks:
+        result_me, result_me_with_extra = count_me_in(talk["q"], talk.get("extra", []))
+        print(talk["q"])
+        print("Me ", result_me)
+        if result_me_with_extra:
+            print("Me and Extra", result_me_with_extra)
+        print()
 
 def main():
     df = read_file()
